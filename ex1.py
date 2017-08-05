5f87dd5c2aeda6f6b65b86ba9db82d23cd108a93
grxssy <30739552+grxssy@users.noreply.github.com>
2017-08-04 21:46:33 -0400

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,14 @@
+#this is a comment
+#write a comment over everything explaining what it does before you run the script and then after
+#to run it open windows powershell and type python3.6 *path to file*
+name = input("What is your name?\n>")
+
+print(f"Hello {name}, I am going to ask you some questions")
+
+age = input("How old are you?\n>")
+height = input("How much do you weigh?\n>")
+weight = input("How tall are you?\n>")
+
+print(f"So you are {age} years old, {height} tall, and {weight} heavy.")
+
+print(f"Goobye, {name}.")
