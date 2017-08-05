efdb00ea1ab99e8fb1da75859f1200aaa29c682e
grxssy <30739552+grxssy@users.noreply.github.com>
2017-08-04 22:14:22 -0400

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,22 @@
+print("How do variables work")
+print("They are easy as shit")
+print("Put an integer or character in and it works")
+
+print("First we will use a whole integer")
+integer = input(">")
+
+print("Now we will use a floating point integer")
+floating = input(">")
+
+print("Finally we will use a character")
+character = input(">")
+
+print(f"Your variables are: {integer}, {floating}, and {character}")
+
+print("We can also do math with integer variables")
+
+integer2 = integer - 2
+integerf = integer + floating
+
+print(f"{integer} - 2 = {integer2}")
+print(f"{integer} + {floating} = {integerf}")
